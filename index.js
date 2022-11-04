7cc42021d65967827fc2174535e602b93c0ef1b6
Alim Ömär <alimjoo00@163.com>
2022-11-05 00:06:55 +0800

    rtl bug fixes

--- index.js
+++ index.js
@@ -18,3 +18,21 @@ function toggleRTL(){
   }
   Preview.Update();
 };
+
+function handleTextDir(){
+  var R_Lvalue = document.querySelector('#outputLatex').getAttribute("dir");
+  var areaval = document.querySelector("textarea").value;
+  var place_holder = document.querySelector("textarea");
+  if (areaval == ''){
+    if(R_Lvalue == 'rtl'){
+      place_holder.setAttribute("dir", "rtl");
+    }else{
+      place_holder.setAttribute("dir", "auto");
+    }
+  }else{
+    if(R_Lvalue != 'auto'){
+      place_holder.setAttribute("dir", "auto");
+    }
+  }
+  Preview.Update();
+}
