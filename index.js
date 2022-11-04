c94ee061240bda503b1bd1f5ca195737c6a970d0
Alim Ömär <alimjoo00@163.com>
2022-11-04 18:28:22 +0800

    dry code

--- index.js
+++ index.js
@@ -4,18 +4,14 @@ function toggleRTL(){
   var dir = to_right.getAttribute("dir");
   if(dir == "ltr"){
     to_right.setAttribute("dir", "rtl");
-    // place_holder.setAttribute("dir", "rtl");
-    to_right.style.fontFamily="chiwer kesme";
-    place_holder.style.fontFamily="Menlo, Monaco, monospace, Ukij Tor";
+    place_holder.setAttribute("dir", "rtl");
     place_holder.setAttribute("placeholder", `قۇر ئىچىدىكى فورمىلا : $e^{i\\pi} + 1 = 0$ 
 <br> 
 قۇر تېشىدىكى فورمىلا : $$ e^{i\\pi} + 1 = 0 $$`);
   }
   else {
       to_right.setAttribute("dir", "ltr");
-      // place_holder.setAttribute("dir", "ltr");
-      to_right.style.fontFamily="Computer Modern";
-      place_holder.style.fontFamily="Menlo, Monaco, 'Courier New', monospace";
+      place_holder.setAttribute("dir", "ltr");
       place_holder.setAttribute("placeholder", `行内公式 : $e^{i\\pi} + 1 = 0$ 
 <br>
 居中公式 : $$ e^{i\\pi} + 1 = 0 $$`);
